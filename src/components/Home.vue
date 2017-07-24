f2e3ebc73a4bec6ed73edd674df425fca5f4abbe
APAC\bolou10 <bo.lou@daimler.com>
2017-07-24 14:46:21 +0800

    rename file

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="hello">
+  <div class="home">
     <h1>{{ msg }}</h1>
     <h2>Ecosystem</h2>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
+  name: 'home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your VTD Dashboard'
     }
   }
 }
