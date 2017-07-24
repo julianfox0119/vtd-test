20489ba6ed655459a3994771bc36bb4d50ff2287
APAC\bolou10 <bo.lou@daimler.com>
2017-07-24 18:13:31 +0800

    test theme

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,14 +1,9 @@
 <template>
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-        <el-menu-item index="1">处理中心</el-menu-item>
-            <el-submenu index="2">
-                <template slot="title">我的工作台</template>
-                <el-menu-item index="2-1">选项1</el-menu-item>
-                <el-menu-item index="2-2">选项2</el-menu-item>
-                <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-submenu>
-        <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-    </el-menu>
+    <div>
+        <header>
+            <img src="../assets/navigation-logo.png" class="header-logo">
+        </header>
+    </div>
 </template>
 
 <script>
@@ -27,7 +22,30 @@ export default {
 }
 </script>
 
-<<style scoped>
-
+<style scoped>
+header {
+    height: 80px;
+    /* position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+    padding: 0 20px;
+    z-index: 1; */
+    text-align: left;
+    box-sizing: border-box;
+    background-color: #000;
+}
+header:after {
+    display: inline-block;
+    content: "";
+    height: 100%;
+    vertical-align: middle;
+}
+.header-logo {
+    display: inline-block;
+    vertical-align: middle;
+    height: 60%;
+    margin-left: 10px;
+}
 </style>
 
