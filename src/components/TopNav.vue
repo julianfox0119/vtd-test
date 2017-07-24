2b98cf4c046a83860f69083d99226b19d6fc13ea
APAC\bolou10 <bo.lou@daimler.com>
2017-07-24 16:21:11 +0800

    change

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -14,6 +14,11 @@
 <script>
 export default {
   name: 'top',
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  },
   methods: {
     handleSelect (key, keyPath) {
       console.log(key, keyPath)
