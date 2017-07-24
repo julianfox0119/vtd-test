64e265923336c62d97823df0d9cf802ad8549718
APAC\bolou10 <bo.lou@daimler.com>
2017-07-24 15:29:02 +0800

    layout page

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,5 +1,7 @@
 <template>
-    <div id="TopNav">Test Top Nav</div>
+    <div class="header">
+        this this header part
+    </div>
 </template>
 
 <script>
@@ -7,3 +9,14 @@ export default {
   name: 'top'
 }
 </script>
+
+<<style scoped>
+.header {
+    z-index: 1;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    color: rgba(255,255,255,0.75);
+    background-color: #24292e;
+}
+</style>
+
