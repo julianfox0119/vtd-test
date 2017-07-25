43a6aa318384bc25241dfc6e253dbc7a9b16eff6
APAC\bolou10 <bo.lou@daimler.com>
2017-07-25 10:19:33 +0800

    test theme

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,13 +2,22 @@
     <div>
         <header>
             <img src="../assets/navigation-logo.png" class="header-logo">
-            <ul class="header-operations">
+            <!-- <ul class="header-operations">
                 <li>帮助</li>
                 <li>
                     <span class="header-lang is-active">中文</span><span>/</span><span class="header-lang">En</span>
                 </li>
-                <!-- <li><el-button type="primary" icon="menu" size="large"></el-button></li> -->
-            </ul>
+            </ul> -->
+            <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                <el-menu-item index="1">帮助</el-menu-item>
+                <el-submenu index="2">
+                    <template slot="title">我的工作台</template>
+                    <el-menu-item index="2-1">选项1</el-menu-item>
+                    <el-menu-item index="2-2">选项2</el-menu-item>
+                    <el-menu-item index="2-3">选项3</el-menu-item>
+                </el-submenu> 
+                <el-menu-item index="2">其它</el-menu-item>
+            </el-menu>
         </header>
     </div>
 </template>
@@ -31,10 +40,10 @@ export default {
 
 <style scoped>
 header {
-    height: 80px;
+    height: 60px;
     text-align: left;
     box-sizing: border-box;
-    background-color: #000;
+    background-color: #1f1f1f;
 }
 .header-operations:after, header:after {
     display: inline-block;
@@ -66,6 +75,9 @@ ul {
     list-style: none outside none;
     margin: 0;
     padding: 0;
+}
+.el-menu-demo{
+    float: right;
 }
 </style>
 
