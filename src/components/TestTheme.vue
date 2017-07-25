a244d30ec561b11a56148596aabb2b6855cf4a07
APAC\bolou10 <bo.lou@daimler.com>
2017-07-25 14:31:21 +0800

    test theme

--- src/components/TestTheme.vue
+++ src/components/TestTheme.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="testTheme">
         <div>Button:
-            <el-button>默认按钮</el-button>
             <el-button type="primary">主要按钮</el-button>
             <el-button type="text">文字按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="primary" :disabled="true">主要按钮</el-button>
         </div>
         <div>Ratio:
             <el-radio class="radio" v-model="radio" label="1">备选项</el-radio>
