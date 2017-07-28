82ffa8eb4f6347a2a930de2207bb44441a0f050c
APAC\bolou10 <bo.lou@daimler.com>
2017-07-28 18:41:02 +0800

    localization

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -55,6 +55,7 @@ export default {
         this.$store.commit('changeLocale', 'en')
         euilocale.use(langEn)
       }
+      this.$router.replace('/')
     },
     handleSelect (key, keyPath) {
     //   console.log(key, keyPath)
