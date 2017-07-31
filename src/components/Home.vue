5c6ecd123e25bd28c48bc7fda0b9908bb7776455
APAC\bolou10 <bo.lou@daimler.com>
2017-07-31 15:18:26 +0800

    test jsonp

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,21 +19,54 @@
     <h2>{{ $t('eco', currentLang) }}</h2>
     <el-button type="primary" @click="clickBtn">{{ $t('btntext', currentLang) }}</el-button>
     <p>{{ $t('hello', currentLang) }}</p>
+    <div class='weather' v-if="weatherInfo">
+      <div><h3>{{weatherInfo.currentCity}} | pm2.5-{{weatherInfo.pm25}} | {{weatherInfo.weather_data.length}}</h3></div>   
+      <el-row type="flex" class="row-bg" justify="center" >
+        <el-col :span="3" v-for="item in weatherInfo.weather_data" :key="item.id" class='detail'>
+          <p title="详情" class='date'>{{item.date.slice(0,10)}}</p>
+          <p>
+            <img :src="item.dayPictureUrl">
+            <img :src="item.nightPictureUrl">
+          </p>
+          <p>{{item.weather}}</p>
+          <p>{{item.wind}}</p>
+          <p>{{item.temperature}}</p>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
+// import axios from 'axios'
+import jsonp from 'jsonp'
+
 export default {
   name: 'home',
   data () {
     return {
       msg: '',
-      currentLang: localStorage.currentLang
+      currentLang: localStorage.currentLang,
+      url: 'http://api.map.baidu.com/telematics/v3/weather?location=北京&output=json&ak=HGOUnCXeQLEeywhGOu2jU29PFdC6duFF',
+      weatherInfo: null
     }
+  },
+  created () {
+    this.getWeather()
   },
   methods: {
     clickBtn: function (event) {
       this.msg = 'Welcome to Your VTD Dashboard'
+    },
+    getWeather () {
+      let self = this
+      jsonp(this.url, null, function (err, data) {
+        if (err) {
+          console.error(err.message)
+        } else {
+          self.weatherInfo = data.results[0]
+        }
+      })
     }
   }
 }
